144e417bd3dbd856d9a6c56a9b3c7d8ac40525d0
Tom MacWright <tom@macwright.org>
2015-02-05 15:34:30 -0500

    Add category

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 var inside = require('turf-inside');
 
 /**
-* Calculates the sum of a field for {@link Point} features within a set of {@link Polygon} features.
-*
-* @module turf/sum
-* @param {FeatureCollection} polygons a FeatureCollection of {@link Polygon} features
-* @param {FeatureCollection} points a FeatureCollection of {@link Point} features
-* @param {String} inField the field in input data to analyze
-* @param {String} outField the field in which to store results
-* @return {FeatureCollection} a FeatureCollection of {@link Polygon} features
-* with properties listed as `outField`
-* @example
-* var polygons = turf.featurecollection([
-*   turf.polygon([[
-*     [-87.990188, 43.026486],
-*     [-87.990188, 43.062115],
-*     [-87.913284, 43.062115],
-*     [-87.913284, 43.026486],
-*     [-87.990188, 43.026486]
-*   ]]),
-*   turf.polygon([[
-*     [-87.973709, 42.962452],
-*     [-87.973709, 43.014689],
-*     [-87.904014, 43.014689],
-*     [-87.904014, 42.962452],
-*     [-87.973709, 42.962452]
-*   ]])
-* ]);
-* var points = turf.featurecollection([
-*   turf.point([-87.974052, 43.049321], {population: 200}),
-*   turf.point([-87.957229, 43.037277], {population: 600}),
-*   turf.point([-87.931137, 43.048568], {population: 100}),
-*   turf.point([-87.963409, 42.99611], {population: 200}),
-*   turf.point([-87.94178, 42.974762], {population: 300})
-* ]);
-*
-* var aggregated = turf.sum(
-*   polygons, points, 'population', 'sum');
-*
-* var result = turf.featurecollection(
-*   points.features.concat(aggregated.features));
-*
-* //=result
-*/
+ * Calculates the sum of a field for {@link Point} features within a set of {@link Polygon} features.
+ *
+ * @module turf/sum
+ * @category aggregation
+ * @param {FeatureCollection} polygons a FeatureCollection of {@link Polygon} features
+ * @param {FeatureCollection} points a FeatureCollection of {@link Point} features
+ * @param {String} inField the field in input data to analyze
+ * @param {String} outField the field in which to store results
+ * @return {FeatureCollection} a FeatureCollection of {@link Polygon} features
+ * with properties listed as `outField`
+ * @example
+ * var polygons = turf.featurecollection([
+ *   turf.polygon([[
+ *     [-87.990188, 43.026486],
+ *     [-87.990188, 43.062115],
+ *     [-87.913284, 43.062115],
+ *     [-87.913284, 43.026486],
+ *     [-87.990188, 43.026486]
+ *   ]]),
+ *   turf.polygon([[
+ *     [-87.973709, 42.962452],
+ *     [-87.973709, 43.014689],
+ *     [-87.904014, 43.014689],
+ *     [-87.904014, 42.962452],
+ *     [-87.973709, 42.962452]
+ *   ]])
+ * ]);
+ * var points = turf.featurecollection([
+ *   turf.point([-87.974052, 43.049321], {population: 200}),
+ *   turf.point([-87.957229, 43.037277], {population: 600}),
+ *   turf.point([-87.931137, 43.048568], {population: 100}),
+ *   turf.point([-87.963409, 42.99611], {population: 200}),
+ *   turf.point([-87.94178, 42.974762], {population: 300})
+ * ]);
+ *
+ * var aggregated = turf.sum(
+ *   polygons, points, 'population', 'sum');
+ *
+ * var result = turf.featurecollection(
+ *   points.features.concat(aggregated.features));
+ *
+ * //=result
+ */
 module.exports = function(polyFC, ptFC, inField, outField){
   polyFC.features.forEach(function(poly){
     if(!poly.properties){
